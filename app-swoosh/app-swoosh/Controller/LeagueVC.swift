be246ebaf8997ce148d52387ec8c6dabb53648ff
Marika <marika@Marikas-MacBook-Air.local>
2017-11-08 16:47:47 +0100

    buttons and logic

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,19 +10,50 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
+    
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
     
+    @IBAction func OnMensTapped(_ sender: Any) {
+       selectLeague(leagueType: "Mens")
+    }
+    
+    
+    @IBAction func OnWomensTapped(_ sender: Any) {
+       selectLeague(leagueType: "Womens")
+    }
+    
+    
+    @IBAction func OnCoedTapped(_ sender: Any) {
+       selectLeague(leagueType: "coed")
+    }
+    
+   
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
+        
     }
 
+    
+    func selectLeague(leagueType: String) {
+       player.desireLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ app-swoosh/app-swoosh/Modal/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Marika on 08/11/2017.
+//  Copyright © 2017 Marika. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desireLeague: String?
+    var selectedSkillLvl: String?
+    
+}
