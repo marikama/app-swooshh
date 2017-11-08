abd6021cdf96e1d522cf3c0f20acb52ee83846aa
Marika <marika@Marikas-MacBook-Air.local>
2017-11-08 15:40:48 +0100

    Controllers added

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,27 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Marika on 08/11/2017.
+//  Copyright © 2017 Marika. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+
+}
---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,26 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Marika on 08/11/2017.
+//  Copyright © 2017 Marika. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
