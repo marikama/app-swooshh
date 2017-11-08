9978618e18f7a16500553e7bc0c2cad97ef0eb0e
Marika <marika@Marikas-MacBook-Air.local>
2017-11-08 15:54:20 +0100

    les07 clean code

--- app-swoosh/app-swoosh/Controller/ViewController.swift
+++ app-swoosh/app-swoosh/Controller/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-    @IBOutlet weak var swoosh: UIImageView!
-    
-    @IBOutlet weak var bgimg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-    swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50,  width:swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        bgimg.frame = view.frame;
-        
     }
 
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
